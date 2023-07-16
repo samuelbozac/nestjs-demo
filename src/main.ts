83f94e117378e5d03c1d881f5d9c38759f7589ef
Samuel Boza <samuelsbozac@gmail.com>
2023-07-16 15:31:31 -0400

    Transform data

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true, 
-      }),
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
   );
   await app.listen(3000);
 }
