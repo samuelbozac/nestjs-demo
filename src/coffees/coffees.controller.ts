fa1f7cc8b7ac4c16ffbc03565674a5c91289dd1c
Samuel Boza <samuelsbozac@gmail.com>
2023-07-28 01:00:29 -0400

    Swagger

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -21,8 +21,8 @@ export class CoffeesController {
 
   @Public()
   @Get()
-  async findAll(@Query() paginationQuery: PaginationQueryDto) {
-    await new Promise(resolve => setTimeout(resolve, 5000));
+  findAll(@Query() paginationQuery: PaginationQueryDto) {
+    // await new Promise(resolve => setTimeout(resolve, 5000));
     return this.coffeesService.findAll(paginationQuery);
   }
 
